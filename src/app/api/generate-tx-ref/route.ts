81c29d087aafee144abacb61102862f9696e09ec
solomon stephen <33923021+realrufans@users.noreply.github.com>
2024-08-09 04:09:10 +0100

    validate

--- src/app/api/generate-tx-ref/route.ts
+++ src/app/api/generate-tx-ref/route.ts
@@ -1,15 +1,12 @@
+export const revalidate = 0;
+
 import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-
-
   const txRef = `txref-${uuidv4()}`;
 
   const response = NextResponse.json({ txRef });
-
-  // Add cache-control headers to prevent caching
-  response.headers.set("Cache-Control", "no-store");
 
   return response;
 }
